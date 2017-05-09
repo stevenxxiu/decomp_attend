e8040310b41cb32eeb224bacea9e1dc38e7be669
Steven Xu <stevenxxiu@gmail.com>
2017-05-10 08:09:38 +1000

    multi-processing sampling, 10s faster per epoch

--- decomp_attend/main.py
+++ decomp_attend/main.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 import joblib
 import numpy as np
 import tensorflow as tf
+from multiprocessing import Queue, Process
 from tensorflow.contrib.tensorboard.plugins import projector
 from tensorflow.python.layers.core import Dense
 from tensorflow.python.ops import init_ops
@@ -74,6 +75,43 @@ def gen_tables(docs):
     return word_to_index
 
 
+def sample(docs, word_to_index, num_unknown, epoch_size, batch_size, q):
+    # sort so sentences have similar lengths according to paper
+    parts = [[], [], []]
+    for i, (sent_1, sent_2, label) in enumerate(docs):
+        if len(sent_1) < 20 and len(sent_2) < 20:
+            parts[0].append(i)
+        elif len(sent_1) < 50 and len(sent_2) < 50:
+            parts[1].append(i)
+        else:
+            parts[2].append(i)
+    for i in range(epoch_size):
+        res = []
+        p = np.concatenate([np.random.permutation(part) for part in parts])
+        for j in range(0, len(p), batch_size):
+            k = p[j:j + batch_size]
+            max_len_1 = max(len(docs[k_][0]) for k_ in k)
+            max_len_2 = max(len(docs[k_][1]) for k_ in k)
+            X_doc_1_ = np.zeros([len(k), max_len_1 + 1], dtype=np.int32)
+            X_doc_2_ = np.zeros([len(k), max_len_2 + 1], dtype=np.int32)
+            mask_1_ = np.zeros([len(k), max_len_1 + 1], dtype=np.float32)
+            mask_2_ = np.zeros([len(k), max_len_2 + 1], dtype=np.float32)
+            y_ = [['contradiction', 'neutral', 'entailment'].index(docs[k_][2]) for k_ in k]
+            for i_k, k_ in enumerate(k):
+                doc_1 = ['\0'] + docs[k_][0]
+                doc_2 = ['\0'] + docs[k_][1]
+                X_doc_1_[i_k, :len(doc_1)] = [
+                    word_to_index.get(word, len(word_to_index) + hash(word) % num_unknown) for word in doc_1
+                ]
+                X_doc_2_[i_k, :len(doc_2)] = [
+                    word_to_index.get(word, len(word_to_index) + hash(word) % num_unknown) for word in doc_2
+                ]
+                mask_1_[i_k, :len(doc_1)] = 1
+                mask_2_[i_k, :len(doc_2)] = 1
+            res.append((X_doc_1_, X_doc_2_, mask_1_, mask_2_, y_))
+        q.put(res)
+
+
 def run_model(docs, word_to_index, num_unknown, embedding_size, dropout_rate, lr, batch_size, epoch_size):
     # special words
     word_to_index['\0'] = len(word_to_index)
@@ -129,47 +167,22 @@ def run_model(docs, word_to_index, num_unknown, embedding_size, dropout_rate, lr
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
+        # start sampling
+        q = Queue(1)
+        Process(target=sample, args=(docs, word_to_index, num_unknown, epoch_size, batch_size, q)).start()
+
         # load pretrained word embeddings
         emb_0 = tf.Variable(0., validate_shape=False)
         saver = tf.train.Saver({'emb': emb_0})
         saver.restore(sess, '__cache__/tf/emb/model.ckpt')
         sess.run(emb[:tf.shape(emb_0)[0]].assign(emb_0))
 
-        # sort so sentences have similar lengths according to paper
-        parts = [[], [], []]
-        for i, (sent_1, sent_2, label) in enumerate(docs):
-            if len(sent_1) < 20 and len(sent_2) < 20:
-                parts[0].append(i)
-            elif len(sent_1) < 50 and len(sent_2) < 50:
-                parts[1].append(i)
-            else:
-                parts[2].append(i)
-
         # train
         print(datetime.datetime.now(), 'started training')
         for i in range(epoch_size):
-            p = np.concatenate([np.random.permutation(part) for part in parts])
             total_loss = 0
-            for j in range(0, len(p), batch_size):
-                k = p[j:j + batch_size]
-                max_len_1 = max(len(docs[k_][0]) for k_ in k)
-                max_len_2 = max(len(docs[k_][1]) for k_ in k)
-                X_doc_1_ = np.zeros([len(k), max_len_1 + 1], dtype=np.int32)
-                X_doc_2_ = np.zeros([len(k), max_len_2 + 1], dtype=np.int32)
-                mask_1_ = np.zeros([len(k), max_len_1 + 1], dtype=np.float32)
-                mask_2_ = np.zeros([len(k), max_len_2 + 1], dtype=np.float32)
-                y_ = [['contradiction', 'neutral', 'entailment'].index(docs[k_][2]) for k_ in k]
-                for i_k, k_ in enumerate(k):
-                    doc_1 = ['\0'] + docs[k_][0]
-                    doc_2 = ['\0'] + docs[k_][1]
-                    X_doc_1_[i_k, :len(doc_1)] = [
-                        word_to_index.get(word, len(word_to_index) + hash(word) % num_unknown) for word in doc_1
-                    ]
-                    X_doc_2_[i_k, :len(doc_2)] = [
-                        word_to_index.get(word, len(word_to_index) + hash(word) % num_unknown) for word in doc_2
-                    ]
-                    mask_1_[i_k, :len(doc_1)] = 1
-                    mask_2_[i_k, :len(doc_2)] = 1
+            res = q.get()
+            for X_doc_1_, X_doc_2_, mask_1_, mask_2_, y_ in res:
                 _, batch_loss = sess.run([train_op, loss], feed_dict={
                     X_doc_1: X_doc_1_, X_doc_2: X_doc_2_, mask_1: mask_1_, mask_2: mask_2_, y: y_, training: True,
                 })

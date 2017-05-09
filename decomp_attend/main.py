6114ebcabb1fd762a49eb5745c49232159861132
Steven Xu <stevenxxiu@gmail.com>
2017-05-10 07:54:25 +1000

    added training code

--- decomp_attend/main.py
+++ decomp_attend/main.py
@@ -1,16 +1,16 @@
 import csv
-import os
+import datetime
 import json
+import os
 import re
 from collections import defaultdict
 
-import numpy as np
 import joblib
+import numpy as np
 import tensorflow as tf
-from tensorflow.contrib.opt import LazyAdamOptimizer
 from tensorflow.contrib.tensorboard.plugins import projector
+from tensorflow.python.layers.core import Dense
 from tensorflow.python.ops import init_ops
-from tensorflow.python.layers.core import Dense, Dropout
 
 memory = joblib.Memory('__cache__', verbose=0)
 
@@ -75,6 +75,9 @@ def gen_tables(docs):
 
 
 def run_model(docs, word_to_index, num_unknown, embedding_size, dropout_rate, lr, batch_size, epoch_size):
+    # special words
+    word_to_index['\0'] = len(word_to_index)
+
     # network
     tf.reset_default_graph()
     X_doc_1 = tf.placeholder(tf.int32, [None, None])
@@ -83,6 +86,7 @@ def run_model(docs, word_to_index, num_unknown, embedding_size, dropout_rate, lr
     mask_2 = tf.placeholder(tf.float32, [None, None])
     y = tf.placeholder(tf.int32, [None])
     training = tf.placeholder(tf.bool, [])
+    batch_size_ = tf.shape(y)[0]
 
     emb = tf.Variable(tf.random_normal([len(word_to_index) + num_unknown, embedding_size], 0, 1))
     emb_1 = tf.nn.embedding_lookup(emb, X_doc_1)
@@ -92,8 +96,8 @@ def run_model(docs, word_to_index, num_unknown, embedding_size, dropout_rate, lr
     attend_d_1 = tf.nn.relu(tf.layers.dropout(l_attend.apply(emb_1), rate=dropout_rate, training=training))
     attend_d_2 = tf.nn.relu(tf.layers.dropout(l_attend.apply(emb_2), rate=dropout_rate, training=training))
     attend_e = tf.matmul(attend_d_1, tf.transpose(attend_d_2, [0, 2, 1]))
-    attend_mask_w_1 = tf.reshape(mask_1, [batch_size, 1, -1])
-    attend_mask_w_2 = tf.reshape(mask_2, [batch_size, 1, -1])
+    attend_mask_w_1 = tf.reshape(mask_1, [batch_size_, 1, -1])
+    attend_mask_w_2 = tf.reshape(mask_2, [batch_size_, 1, -1])
     # attend_norm_1 are the weights to attend for in sentence 2 for each word of sentence 1
     attend_norm_1 = tf.nn.softmax(attend_mask_w_2 * attend_e + (-1 / attend_mask_w_2 + 1))
     attend_norm_2 = tf.nn.softmax(attend_mask_w_1 * tf.transpose(attend_e, [0, 2, 1]) + (-1 / attend_mask_w_1 + 1))
@@ -108,44 +112,69 @@ def run_model(docs, word_to_index, num_unknown, embedding_size, dropout_rate, lr
         tf.concat([emb_2, attend_2], 2)
     ), rate=dropout_rate, training=training))
 
-    agg_1 = tf.reduce_sum(tf.reshape(mask_1, [batch_size, -1, 1]) * compare_1, 1)
-    agg_2 = tf.reduce_sum(tf.reshape(mask_2, [batch_size, -1, 1]) * compare_2, 1)
+    agg_1 = tf.reduce_sum(tf.reshape(mask_1, [batch_size_, -1, 1]) * compare_1, 1)
+    agg_2 = tf.reduce_sum(tf.reshape(mask_2, [batch_size_, -1, 1]) * compare_2, 1)
     logits = tf.nn.relu(tf.layers.dropout(tf.layers.dense(
         tf.concat([agg_1, agg_2], 1), 200, kernel_initializer=init_ops.RandomNormal(0, 0.01)
     ), rate=dropout_rate, training=training))
     logits = tf.layers.dense(logits, 3, kernel_initializer=init_ops.RandomNormal(0, 0.01))
     loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y))
-    train_op = LazyAdamOptimizer(learning_rate=lr).minimize(loss)
 
-    # XXX do not train embeddings
+    # we do not need LazyAdamOptimizer since embeddings are not updated
+    opt = tf.train.AdamOptimizer(learning_rate=lr)
+    grads = opt.compute_gradients(loss)
+    train_op = opt.apply_gradients([(grad, var) for grad, var in grads if var != emb])
 
     # run
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        pass
 
-        # XXX prepend each sentence with null token
-        # XXX sort so sentences have similar lengths according to paper
+        # load pretrained word embeddings
+        emb_0 = tf.Variable(0., validate_shape=False)
+        saver = tf.train.Saver({'emb': emb_0})
+        saver.restore(sess, '__cache__/tf/emb/model.ckpt')
+        sess.run(emb[:tf.shape(emb_0)[0]].assign(emb_0))
 
-        # # train
-        # print(datetime.datetime.now(), 'started training')
-        # q = Queue(1)
-        # Process(target=dbow_sample, args=(docs, word_to_index, word_to_freq, sample, epoch_size, q)).start()
-        # for i in range(epoch_size):
-        #     X_doc_, y_ = q.get()
-        #     p = np.random.permutation(len(y_))
-        #     total_loss = 0
-        #     for j in range(0, len(y_), batch_size):
-        #         k = p[j:j + batch_size]
-        #         _, batch_loss = sess.run([train_op, loss], feed_dict={X_doc: X_doc_[k], y: y_[k]})
-        #         total_loss += batch_loss
-        #     print(datetime.datetime.now(), f'finished epoch {i}, loss: {total_loss / len(y_):f}')
-        #
-        # # save
-        # path = os.path.join('__cache__', 'tf', f'dbow-{name}-{uuid.uuid4()}')
-        # os.makedirs(path)
-        # save_model(path, docs, word_to_index, word_to_freq, emb_doc, None, l.W, sess)
-        # return path
+        # sort so sentences have similar lengths according to paper
+        parts = [[], [], []]
+        for i, (sent_1, sent_2, label) in enumerate(docs):
+            if len(sent_1) < 20 and len(sent_2) < 20:
+                parts[0].append(i)
+            elif len(sent_1) < 50 and len(sent_2) < 50:
+                parts[1].append(i)
+            else:
+                parts[2].append(i)
+
+        # train
+        print(datetime.datetime.now(), 'started training')
+        for i in range(epoch_size):
+            p = np.concatenate([np.random.permutation(part) for part in parts])
+            total_loss = 0
+            for j in range(0, len(p), batch_size):
+                k = p[j:j + batch_size]
+                max_len_1 = max(len(docs[k_][0]) for k_ in k)
+                max_len_2 = max(len(docs[k_][1]) for k_ in k)
+                X_doc_1_ = np.zeros([len(k), max_len_1 + 1], dtype=np.int32)
+                X_doc_2_ = np.zeros([len(k), max_len_2 + 1], dtype=np.int32)
+                mask_1_ = np.zeros([len(k), max_len_1 + 1], dtype=np.float32)
+                mask_2_ = np.zeros([len(k), max_len_2 + 1], dtype=np.float32)
+                y_ = [['contradiction', 'neutral', 'entailment'].index(docs[k_][2]) for k_ in k]
+                for i_k, k_ in enumerate(k):
+                    doc_1 = ['\0'] + docs[k_][0]
+                    doc_2 = ['\0'] + docs[k_][1]
+                    X_doc_1_[i_k, :len(doc_1)] = [
+                        word_to_index.get(word, len(word_to_index) + hash(word) % num_unknown) for word in doc_1
+                    ]
+                    X_doc_2_[i_k, :len(doc_2)] = [
+                        word_to_index.get(word, len(word_to_index) + hash(word) % num_unknown) for word in doc_2
+                    ]
+                    mask_1_[i_k, :len(doc_1)] = 1
+                    mask_2_[i_k, :len(doc_2)] = 1
+                _, batch_loss = sess.run([train_op, loss], feed_dict={
+                    X_doc_1: X_doc_1_, X_doc_2: X_doc_2_, mask_1: mask_1_, mask_2: mask_2_, y: y_, training: True,
+                })
+                total_loss += batch_loss
+            print(datetime.datetime.now(), f'finished epoch {i}, loss: {total_loss / len(y_):f}')
 
 
 def main():
@@ -153,9 +182,8 @@ def main():
     word_to_index = gen_tables(train)
     run_model(
         train, word_to_index=word_to_index, num_unknown=100, embedding_size=300, dropout_rate=0.2, lr=0.01,
-        batch_size=2, epoch_size=360
+        batch_size=512, epoch_size=360
     )
-
 
 if __name__ == '__main__':
     main()

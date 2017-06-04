01d839338cc32c31ca7eda3c9326e661bd3cadd4
Steven Xu <stevenxxiu@gmail.com>
2017-06-04 16:09:06 +1000

    export PYTHONUNBUFFERED=1

--- jobscript.sh
+++ jobscript.sh
@@ -6,7 +6,10 @@
 #PBS -l walltime=20:00:00
 module load tensorflow/1.0.1-python3.5
 cd /short/cp1/sx6361/decomp_attend
-/home/563/sx6361/.pyenv/versions/3.6.1/bin/python decomp_attend/main.py '{
+python=/home/563/sx6361/.pyenv/versions/3.6.1/bin/python
+export PYTHONPATH="/home/563/sx6361/.pyenv/versions/3.6.1/lib/python3.6/site-packages:$PWD"
+export PYTHONUNBUFFERED=1
+"$python" decomp_attend/main.py '{
     "intra_sent": true, "emb_unknown": 100, "emb_size": 300,
     "emb_proj": 200, "emb_proj_pca": false, "emb_normalize": true,
     "n_intra": [200, 200], "n_intra_bias": 10, "n_attend": [200, 200], "n_compare": [200, 200], "n_classif": [200, 200],
